00f1b2109d6a3947042f1d4c412e5bfd1e8552a9
didar <zhagipardidarit@gmail.com>
2026-04-27 11:17:29 +0500

    added Main Stream

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Main entry point for the University Management System.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to University Management System!");
+
+    }
+}
